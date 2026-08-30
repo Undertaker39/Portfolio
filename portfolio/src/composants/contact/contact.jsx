ecc0ede97a4ace4291ca4dcdd355ae249570cdec
Undertaker39 <p.balp39@gmail.com>
2026-08-30 12:15:57 +0200

    mailto

--- portfolio/src/composants/contact/contact.jsx
+++ portfolio/src/composants/contact/contact.jsx
@@ -8,13 +8,9 @@ function Contact() {
                 <p>Je suis joignable sur :</p>
                 <ul className='tel'>
                     <li><i className="fa-solid fa-mobile"></i> 06.87.84.18.17</li>
-                    <li><i className="fa-solid fa-clock"></i> De 8h à 17h</li>
-                    <li><i className="fa-regular fa-calendar-days"></i>5 J/7</li>
                 </ul>
                 <ul className='mail'>
-                    <li><i className="fa-solid fa-at"></i> p.balp39@gmail.com</li>
-                    <li><i className="fa-solid fa-clock"></i> H24</li>
-                    <li><i className="fa-regular fa-calendar-days"></i>7 J/7</li>
+                    <a href='mailto:p.balp39@gmail.com'><li><i className="fa-solid fa-at"></i> p.balp39@gmail.com</li></a>
                 </ul>
             </article>
         </section>
